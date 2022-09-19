9b33650dcb20e7c087d71e6f2b2c98921d3b84fa
Keks <keks@htmlacademy.ru>
2022-09-19 17:37:17 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,24 @@
+function getRandomNumber(startNumber, finalNumber) {
+  if (startNumber < 0 || finalNumber < 0) {
+    return 'Некорректный диапазон';
+  }
+
+  if (Math.ceil(startNumber) === Math.floor(finalNumber)) {
+    return Math.ceil(startNumber);
+  }
+
+  if (startNumber > finalNumber) {
+    const twist = startNumber;
+    startNumber = finalNumber;
+    finalNumber = twist;
+  }
+  const startNum = Math.ceil(startNumber);
+  return Math.floor(Math.random() * (Math.floor(finalNumber) - startNum + 1)) + startNum;
+
+}
+getRandomNumber(0.3, 7);
+
+function maxLength(string, maxLen) {
+  return string.length <= maxLen;
+}
+maxLength('', 4);

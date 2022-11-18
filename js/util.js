5a82aa21f19dbf98a4736f8efa97b1964297516f
koma-4 <koma_ni@mail.ru>
2022-11-18 15:14:19 +0300

    module8-task2

--- js/util.js
+++ js/util.js
@@ -19,4 +19,6 @@ function getNumID2() {
 
 const isEscKey = (keyCode) => keyCode === 'Escape';
 
-export {getRandomNumber, getNumID1, getNumID2, getRandomArrayElement, isEscKey};
+const createWordToNumber = (number, words) => words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? Math.abs(number) % 10 : 5]];
+
+export {getRandomNumber, getNumID1, getNumID2, getRandomArrayElement, isEscKey, createWordToNumber};

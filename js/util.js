0a114badfbff7cb23629dd2f83944fa341cc75ae
koma-4 <koma_ni@mail.ru>
2022-11-24 17:10:31 +0300

    module9-task1

--- js/util.js
+++ js/util.js
@@ -21,4 +21,26 @@ const isEscKey = (keyCode) => keyCode === 'Escape';
 
 const createWordToNumber = (number, words) => words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? Math.abs(number) % 10 : 5]];
 
-export {getRandomNumber, getNumID1, getNumID2, getRandomArrayElement, isEscKey, createWordToNumber};
+const ALERT_SHOW_TIME = 5000;
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+export {getRandomNumber, getNumID1, getNumID2, getRandomArrayElement, isEscKey, createWordToNumber, showAlert};
